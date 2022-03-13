6d03a1321ea52bd3cd8e4318d0f73348c5971e67
liululu <lu_lu_liu@163.com>
2022-03-13 23:40:01 +0800

    add 2.2.1 sgi allocator

--- code/chapter2/2.2.1/sgi_allocator/sgi_allocator/main.cpp
+++ code/chapter2/2.2.1/sgi_allocator/sgi_allocator/main.cpp
@@ -1,15 +1,13 @@
-#include "jjalloc.h"
+#include "def_alloc.h"
 #include <vector>
-#include <iostream>
 
 
 using namespace std;
 
-
 int main()
 {
 	int ia[5] = { 0,1,2,3,4 };
-	vector<int, JJ::allocator<int>> iv(ia, ia + 5);
+	vector<int, ::allocator<int>> iv(ia, ia + 5);
 	for (int i = 0; i < iv.size(); ++i)
 	{
 		cout << iv[i] << " ";
